9f9a272f8a33b846fd3dd5f4e92a6f45e138b3b2
Pol De Puysseleyr <pol.depuysseleyr.v7467@student.hogent.be>
2015-02-10 12:00:30 +0100

    gitignore gewijzigd : vp uitgesloten jongeu dees is voor u

--- src/domein/HelloWorld.java
+++ src/domein/HelloWorld.java
@@ -12,6 +12,7 @@ package domein;
 public class HelloWorld {
     public static void main(String args[]){
         System.out.println("Hello world");
+        
      
                                                 
                 
