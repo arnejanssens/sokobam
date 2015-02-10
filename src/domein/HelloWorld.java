6c00464b281df4b994706c926527b432f904aa4f
Pol De Puysseleyr <pol.depuysseleyr.v7467@student.hogent.be>
2015-02-10 11:29:27 +0100

    gitignore gewijzigd : vp uitgesloten jongeu dees is voor u

--- src/domein/HelloWorld.java
+++ src/domein/HelloWorld.java
@@ -12,11 +12,7 @@ package domein;
 public class HelloWorld {
     public static void main(String args[]){
         System.out.println("Hello world");
-        System.out.println("Arne ik hou van je");
-        System.out.println("love you to (no homo)");
-        System.out.println("Heb je een grote?");
-        System.out.println("droog in de kakker");
-        System.out.println("wil je mijn grote meneer ook is pullen vuile homoslet");
+        
         
     }
 }
